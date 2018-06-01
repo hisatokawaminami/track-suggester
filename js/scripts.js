65e5edd56248f12e866060910d945999b05b6460
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-06-01 12:57:00 -0700

    add alert if age is  blank

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+
     var yourName = $("input#yourName").val();
+    var age = parseInt($("input#age").val());
+
+    if (age) {
+      
+        } else {
+          alert('Please enter your age.');
+        }
+
 
 
     $(".yourName").text(yourName);
+
+
+
 
 
     $("#ruby, #cSharp, #css").show();
