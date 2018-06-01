094b37b97afd5438306923d1f3ddf87fd487a5a3
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-06-01 14:55:44 -0700

    add if statement for q1 and q2

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,34 @@ $(document).ready(function() {
 
     var yourName = $("input#yourName").val();
     var age = parseInt($("input#age").val());
+    var computerType = $("input:radio[name=computerType]:checked").val();
+    var rate = parseInt($("#rate").val());
 
-    if (age) {
-      
-        } else {
-          alert('Please enter your age.');
-        }
-
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    var total = question1 + question2;
 
 
     $(".yourName").text(yourName);
 
+    if (total === 2) {
+        $("#ruby").show();
+        $("#css").hide();
+        $("#cSharp").hide();
+    }
+    else if (total === 3) {
+        $("#ruby").hide();
+        $("#css").show();
+        $("#cSharp").hide();
+    }
+    else if (total === 4) {
+        $("#ruby").hide();
+        $("#css").hide();
+        $("#cSharp").show();
+    }
 
 
 
-
-    $("#ruby, #cSharp, #css").show();
 
   });
 });
